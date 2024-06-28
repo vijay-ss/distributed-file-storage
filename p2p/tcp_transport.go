28823995ad7f38f6c0d0ca633db656d53eee51cb
vijay-ss <54510959+vijay-ss@users.noreply.github.com>
2024-06-27 23:40:46 -0400

    add RPC channel (#4)
    * feat: add RPC channel
    
    * feat: configure OnPeer func
    
    * feat: add file store functionality
    
    ---------
    
    Co-authored-by: Vijay Saddi <vijaysaddi@vijays-mbp.home>

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -3,7 +3,6 @@ package p2p
 import (
 	"fmt"
 	"net"
-	"sync"
 )
 
 // TCPPeer represents the remote node over a TCP established connection.
@@ -23,24 +22,36 @@ func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
 	}
 }
 
+// Close implements the Peer interface.
+func(p *TCPPeer) Close() error {
+	return p.conn.Close()
+}
+
 type TCPTransportOpts struct {
 	ListenAddr string
 	HandshakeFunc HandshakeFunc
 	Decoder Decoder
+	OnPeer func(Peer) error
 }
 
 type TCPTransport struct {
 	TCPTransportOpts
 	listener net.Listener
-
-	mu sync.RWMutex
-	peers map[net.Addr]Peer
+	rpcch chan RPC
 }
 
 func NewTCPTransport(opts TCPTransportOpts) *TCPTransport {
 	return &TCPTransport{
 		TCPTransportOpts: opts,
+		rpcch: make(chan RPC),
 	}
+}
+
+// Consume implements the Transport interface, which will
+// return a read-only channel for the incoming messages
+// received from another peer in the network.
+func (t *TCPTransport) Consume() <-chan RPC {
+	return t.rpcch
 }
 
 func (t *TCPTransport) ListenAndAccept() error {
@@ -72,6 +83,11 @@ func (t *TCPTransport) startAcceptLoop() {
 type Temp struct{}
 
 func (t *TCPTransport) handleConn(conn net.Conn) {
+	var err error
+	defer func() {
+		fmt.Printf("dropping peer connection: %s", err)
+	}()
+
 	peer := NewTCPPeer(conn, true)
 
 	if err := t.HandshakeFunc(peer); err != nil {
@@ -79,18 +95,23 @@ func (t *TCPTransport) handleConn(conn net.Conn) {
 		fmt.Printf("TCP handshake error: %s\n", err)
 		return
 	}
+
+	if t.OnPeer != nil {
+		if err := t.OnPeer(peer); err != nil {
+			return
+		}
+	}
 	
 	// Read loop
-	msg := &Message{}
+	rpc := RPC{}
 	for {
-		if err := t.Decoder.Decode(conn, msg); err != nil {
-			fmt.Printf("TCP error: %s\n", err)
-			continue
+		err = t.Decoder.Decode(conn, &rpc)
+		if err != nil {
+			return
 		}
 
-		msg.From = conn.RemoteAddr()
-
-		fmt.Printf("Message: %+v\n", msg)
+		rpc.From = conn.RemoteAddr()
+		t.rpcch <- rpc
 	}
 
 }
---
+++ store.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+type PathTransformFunc func(string) string
+
+type StoreOpts struct {
+	PathTransformFunc PathTransformFunc
+}
+
+var DefaultPathTransformFunc = func(key string) string {
+	return key
+}
+
+type Store struct {
+	StoreOpts
+}
+
+func NewStore(opts StoreOpts) *Store {
+	return &Store{
+		StoreOpts: opts,
+	}
+}
+
+func (s *Store) writeStream(key string, r io.Reader) error {
+	pathName := s.PathTransformFunc(key)
+	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
+		return err
+	}
+
+	fileName := "someFileName"
+	pathAndfileName := pathName + "/" + fileName
+
+	f, err := os.Create(pathAndfileName)
+	if err != nil {
+		return err
+	}
+
+	n, err := io.Copy(f, r)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("written (%d) bytes to disk %s", n, pathAndfileName)
+
+	return nil
+}
